005d73f284a511c39ca4b3a765d4f29e51c374bf
MrErenK <akbaseren4751@gmail.com>
2023-02-14 19:56:09 +0300

    Scripts: kranul-build: Add `CLANG_TRIPLE` flag back
    * Makefile:508: *** "Clang with Android --target detected. Did you specify CLANG_TRIPLE?".  Stop.
    
    Signed-off-by: MrErenK <akbaseren4751@gmail.com>

--- kranul-build.sh
+++ kranul-build.sh
@@ -121,6 +121,7 @@ make -j"$CORES" ARCH=arm64 O=out \
     OBJCOPY=llvm-objcopy \
     OBJDUMP=llvm-objdump \
     STRIP=llvm-strip \
+    CLANG_TRIPLE=aarch64-linux-gnu- \
     CROSS_COMPILE=aarch64-linux-android- \
     CROSS_COMPILE_ARM32=arm-linux-androideabi- \
     2>&1 | tee "${BUILD_LOG}"
