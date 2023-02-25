2d06e2a3da676385b889e49bc64c5b090dcc15e7
Neebeン <neebexd@gmail.com>
2023-02-26 01:29:51 +0700

    Scripts: kranul-build: Update Copyright
    Signed-off-by: MrErenK <akbaseren4751@gmail.com>

--- kranul-build.sh
+++ kranul-build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright (C) 2022 Neebe3289 <neebexd@gmail.com>
+# Copyright (C) 2022-2023 Neebe3289 <neebexd@gmail.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
