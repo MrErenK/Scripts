6b83d8c5bd2c2c7b0c924eb77da49316e91a036e
MrErenK <akbaseren4751@gmail.com>
2023-07-09 15:35:17 +0300

    Scripts: setup-aosp-env: Install repo package * For temporary, you'd better use newer repo that comes when you init a manifest repository. Example: sudo cp /home/EreN/void/.repo/repo/repo /usr/bin/repo && sudo chmod +x /usr/bin/repo
    Signed-off-by: MrErenK <akbaseren4751@gmail.com>

--- README.md
+++ README.md
@@ -6,6 +6,6 @@
 - Check config.env, do your changes and run kranul-build.sh
 
 *To use setup aosp env script*:
-- run `wget https://raw.githubusercontent.com/MrErenK/Scripts/main/setup-aosp-environment.sh && chmod +x ./setup-aosp-environment.sh`
+- run `wget https://raw.githubusercontent.com/MrErenK/Scripts/main/setup-aosp-environment.sh && chmod +x ./setup-aosp-environment.sh && ./setup-aosp-environment.sh`
 
 It's better to use those scripts with Ubuntu 22.04+
--- setup-aosp-environment.sh
+++ setup-aosp-environment.sh
@@ -8,14 +8,14 @@ function installpkgs() {
     DEBIAN_FRONTEND=noninteractive apt-get install -y python3 bc bison build-essential curl ccache coreutils flex git gnupg gperf lib32z1-dev liblz4-tool \
     libncurses5-dev libsdl1.2-dev libwxgtk3.0-gtk3-dev imagemagick lunzip lzop schedtool squashfs-tools xsltproc zip zlib1g-dev perl xmlstarlet virtualenv xz-utils rr jq libncurses5 pngcrush \
     lib32ncurses5-dev git-lfs libxml2 openjdk-11-jdk wget lib32readline-dev libxml2-utils android-sdk-libsparse-utils lld gcc-multilib g++-multilib libc6-dev-i386 \
-    x11proto-core-dev libx11-dev libgl1-mesa-dev unzip fontconfig ca-certificates bc cpio bsdmainutils lz4 aria2 rclone ssh-client libssl-dev rsync python-is-python3 libarchive-tools
+    x11proto-core-dev libx11-dev libgl1-mesa-dev unzip fontconfig ca-certificates bc cpio bsdmainutils lz4 aria2 rclone ssh-client libssl-dev rsync python-is-python3 libarchive-tools repo
   else
     echo "[!] sudo command found... Using it to install packages..."
     sudo DEBIAN_FRONTEND=noninteractive apt update -yqq
     sudo DEBIAN_FRONTEND=noninteractive apt-get install -y python3 bc bison build-essential curl ccache coreutils flex git gnupg gperf lib32z1-dev liblz4-tool \
     libncurses5-dev libsdl1.2-dev libwxgtk3.0-gtk3-dev imagemagick lunzip lzop schedtool squashfs-tools xsltproc zip zlib1g-dev perl xmlstarlet virtualenv xz-utils rr jq libncurses5 pngcrush \
     lib32ncurses5-dev git-lfs libxml2 openjdk-11-jdk wget lib32readline-dev libxml2-utils android-sdk-libsparse-utils lld gcc-multilib g++-multilib libc6-dev-i386 \
-    x11proto-core-dev libx11-dev libgl1-mesa-dev unzip fontconfig ca-certificates bc cpio bsdmainutils lz4 aria2 rclone ssh-client libssl-dev rsync python-is-python3 libarchive-tools
+    x11proto-core-dev libx11-dev libgl1-mesa-dev unzip fontconfig ca-certificates bc cpio bsdmainutils lz4 aria2 rclone ssh-client libssl-dev rsync python-is-python3 libarchive-tools repo
   fi
 }
 
