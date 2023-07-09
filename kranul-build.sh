9a499d5c67cc0a863837b32e7e428e033768738d
MrErenK <akbaseren4751@gmail.com>
2023-07-09 10:21:29 +0300

    Scripts: kranul-build: Add KernelSU Version info to anykernel script
    Signed-off-by: MrErenK <akbaseren4751@gmail.com>

--- kranul-build.sh
+++ kranul-build.sh
@@ -339,7 +339,11 @@ make -j"$CORES" ARCH=$ARCH O=out \
 # Zipping function
 function zipping() {
     cd ${AnyKernelPath} || exit 1
-    sed -i "s/kernel.string=.*/kernel.string=${KERNEL_NAME}-${SUBLEVEL}-${KERNEL_VARIANT} by ${KBUILD_BUILD_USER} for ${DEVICE_MODEL} (${DEVICE_CODENAME})/g" anykernel.sh
+    if [ "$KERNELSU" = "yes" ];then
+      sed -i "s/kernel.string=.*/kernel.string=${KERNEL_NAME} ${SUBLEVEL} ${KERNEL_VARIANT} by ${KBUILD_BUILD_USER} for ${DEVICE_MODEL} (${DEVICE_CODENAME}) | KernelSU Version: ${KERNELSU_VERSION}/g" anykernel.sh
+    else
+      sed -i "s/kernel.string=.*/kernel.string=${KERNEL_NAME} ${SUBLEVEL} ${KERNEL_VARIANT} by ${KBUILD_BUILD_USER} for ${DEVICE_MODEL} (${DEVICE_CODENAME})/g" anykernel.sh
+    fi
     zip -r9 "[${KERNEL_VARIANT}]"-${KERNEL_NAME}-${SUBLEVEL}-${DEVICE_CODENAME}.zip * -x .git README.md *placeholder
     cd ..
     mkdir -p builds
